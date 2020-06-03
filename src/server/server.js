ad59c70a7a45fb4ccaf755e9d038e2ee28687f4a
Solid-Peace <fhound.p@gmail.com>
2020-06-03 10:42:13 +0200

    api scaffold done

--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const logger = require('./middleware/logger');
+const userRoutes = require('./routes/api/user');
 
 let app = express();
 
+// Serve statics files
 app.use(express.static('public'));
 app.use(express.static('dist'));
 
+// To see incoming request
+app.use(logger);
+
+// API routes to Users data Handling
+app.use('/api/user/', userRoutes);
+
+
+// Application entry
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-    console.log('server listening');
-});
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`) );
